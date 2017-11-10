4f7188b48b220e2b952832023c08f5549f0c1e7d
whatislife <1053400192@qq.com>
2017-11-10 10:24:31 +0800

    基础提交

---
+++ src/main/java/com/mybatis/mybatis_generatorsqlmap/App.java
@@ -0,0 +1,13 @@
+package com.mybatis.mybatis_generatorsqlmap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
